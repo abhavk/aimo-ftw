6b633c4b1ce533015c1cae30ac7487c0848bf69c
Abhav Kedia <abhavkedia@gmail.com>
2024-06-20 18:34:57 -0700

    basic working version

---
+++ huggingface_api.py
@@ -0,0 +1,165 @@
+import requests
+from prompts import code_prompt
+import transformers
+import torch
+import gc
+
+API_URL = "https://li1t6zzkr7cn9h8f.us-east-1.aws.endpoints.huggingface.cloud"
+RL_API_URL = "https://tcou0ujy480brhb5.us-east-1.aws.endpoints.huggingface.cloud"
+headers = {
+	"Accept" : "application/json",
+	"Authorization": "Bearer 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",
+	"Content-Type": "application/json" 
+}
+
+MODEL_PATH = "/kaggle/input/deepseek-math"
+
+from transformers import (
+    AutoModelForCausalLM, 
+    AutoTokenizer, 
+    AutoConfig,
+    StoppingCriteria,
+    StoppingCriteriaList,
+    set_seed
+)
+
+config = AutoConfig.from_pretrained(MODEL_PATH)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+
+device_map = [('model.embed_tokens', 0),
+                 ('model.layers.0', 0),
+                 ('model.layers.1', 0),
+                 ('model.layers.2', 0),
+                 ('model.layers.3', 0),
+                 ('model.layers.4', 0),
+                 ('model.layers.5', 0),
+                 ('model.layers.6', 0),
+                 ('model.layers.7', 0),
+                 ('model.layers.8', 0),
+                 ('model.layers.9', 0),
+                 ('model.layers.10', 0),
+                 ('model.layers.11', 0),
+                 ('model.layers.12', 0),
+                 ('model.layers.13', 0),
+                 ('model.layers.14', 0),
+                 ('model.layers.15', 0),
+                 ('model.layers.16', 0),
+                 ('model.layers.17', 0),
+                 ('model.layers.18', 0),
+                 ('model.layers.19', 0),
+                 ('model.layers.20', 0),
+                 ('model.layers.21', 0),
+                 ('model.layers.22', 1),
+                 ('model.layers.23', 1),
+                 ('model.layers.24', 1),
+                 ('model.layers.25', 1),
+                 ('model.layers.26', 1),
+                 ('model.layers.27', 1),
+                 ('model.layers.28', 1),
+                 ('model.layers.29', 1),
+                 ('model.norm', 1),
+                 ('lm_head', 1)]
+
+device_map = {ii:jj for (ii,jj) in device_map}
+
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_PATH,
+    device_map=device_map,
+    torch_dtype="auto",
+    trust_remote_code=True,
+    #quantization_config=quantization_config,
+    config=config
+)
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype='auto',
+    device_map=device_map,
+)
+
+
+class StoppingCriteriaSub(StoppingCriteria):
+    def __init__(self, stops = [], encounters=1):
+        super().__init__()
+        self.stops = [stop.to("cuda") for stop in stops]
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor):
+        for stop in self.stops:
+            last_token = input_ids[0][-len(stop):]
+            if torch.all(torch.eq(stop,last_token)):
+                return True
+        return False
+
+def query(payload):
+	response = requests.post(RL_API_URL, headers=headers, json=payload)
+	return response.json()
+
+def generate_response(text, type, max_new_tokens, old_key_values=None, local=False):
+    if local:
+        return generate_response_local(text, type, max_new_tokens, old_key_values)
+    else:
+        return generate_response_api(text, type, max_new_tokens)
+    
+APPROACH_STOP_WORDS = ["```output", "```python", "```\nOutput" , ")\n```" , "``````output", "``````python"] #,  
+CODE_STOP_WORDS = ["```output", ")\n```", "```\nOutput" , ")\n```" , "``````output"]
+    
+def generate_response_local(text, type, max_new_tokens, old_key_values=None):
+    stop_words = None
+    if type == "approach":
+        stop_words = APPROACH_STOP_WORDS
+    else:
+        stop_words = CODE_STOP_WORDS
+
+    stop_words_ids = [tokenizer(stop_word, return_tensors='pt', add_special_tokens=False)['input_ids'].squeeze() for stop_word in stop_words]
+    stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
+    temperature = 0.9
+    top_p = 1.0
+    model_inputs = tokenizer(text, return_tensors='pt').to(model.device)
+    input_len = len(model_inputs['input_ids'][0])
+    generation_output = model.generate(**model_inputs, 
+        max_new_tokens=max_new_tokens,
+        return_dict_in_generate=True,
+        past_key_values = old_key_values,
+        do_sample = True,
+        temperature = temperature,
+        top_p = top_p,
+        num_return_sequences=1, stopping_criteria = stopping_criteria
+    )
+    output_ids = generation_output.sequences[0]
+    old_key_values = generation_output.old_key_values
+
+    decoded_output = tokenizer.decode(output_ids, skip_special_tokens=True)
+
+    stop_word_cond = False
+    for stop_word in stop_words:
+        stop_word_cond = stop_word_cond or (decoded_output[-len(stop_word):]==stop_word)
+
+    return decoded_output, stop_word_cond, old_key_values
+
+def generate_response_api(problem, type, max_new_tokens=1042):
+    if type == "approach":
+        template = """User: Below is a math problem you must solve (non-negative numerical answer):\n{}\nWrite the entire script covering all the steps (use comments and document it well) and print the result. After solving the problem, output the final numerical answer within \\boxed{{}}.\n\nApproach:"""
+    elif type == "code":
+        template = code_prompt
+    else:
+        print("Invalid type")
+    inputs = template.format(problem)
+    print(f"Input: {inputs}")
+    output = query({
+        "inputs": inputs,
+        "parameters": {
+            "max_new_tokens": max_new_tokens,
+            "use_cache": False,
+            "return_full_text": False,
+            "temperature": 0.69
+        }
+    })
+    return output
+
+if __name__ == "__main__":
+    # read problem from input
+    problem = input()
+    response = generate_response(problem)
+    print(response)
---
+++ training_runall.py
@@ -0,0 +1,119 @@
+# import from other files
+from huggingface_api import generate_response
+from response_processing import process_text_output, process_code
+import csv
+import tqdm
+
+def sample_best_answer(answers):
+    # get the most frequent answer that isn't negative
+    answer_counts = {}
+    for answer in answers:
+        if answer > 0:
+            if answer in answer_counts:
+                answer_counts[answer] += 1
+            else:
+                answer_counts[answer] = 1
+    
+    if len(answer_counts) == 0:
+        return -1
+    
+    best_answer = max(answer_counts, key=answer_counts.get)
+    return best_answer
+
+def predict(problem):
+    n_repetitions = 15
+    answers = []
+    for i in tqdm(range(n_repetitions)):
+        start_text = """Below is a math problem you are to solve (non-negative numerical answer):
+\"{}\"
+To accomplish this, first determine a sympy-based approach for solving the problem by listing each step to take and what functions need to be called in each step. Then write any code necessary in your solution. Iterate between thoughts and code until you arrive at a solution. Put your final numerical answer within \\boxed{}\\.
+Note that while the intermediate outputs may be real numbers, the final answer will always be a numerical value."""
+
+        MAX_TOKENS = 2048
+        cumulative_text = start_text.format(problem)
+        ALREADY_GENERATED = len(cumulative_text)
+        # start with approach
+        NEXT_GEN = "approach"
+        
+        while (ALREADY_GENERATED < MAX_TOKENS - 100):
+            if NEXT_GEN == "approach":
+                cumulative_text += "\n\nApproach:"
+            elif NEXT_GEN == "code":
+                cumulative_text += "\n\n```python"
+
+            # TODO: This is loose since words and tokens aren't correlated directly
+            remaining_words = MAX_TOKENS-len(cumulative_text)
+            try: 
+                decoded_output, stop_word_cond, old_key_values = generate_response(cumulative_text, NEXT_GEN, remaining_words, local=True)
+            except Exception as e:
+                print(f"\033[91mError: {e}\033[0m")
+
+            if stop_word_cond:
+                print(f"Stopped: Stop word encountered.")
+            else:
+                print(f"Stopped: End of generation.")
+
+            generation = decoded_output[len(cumulative_text):]
+            print(f"Generated: {generation}")
+
+            if NEXT_GEN == "approach":
+                maybe_answer = process_text_output(generation)
+                if maybe_answer > 0:
+                    print(f"Answer found: {maybe_answer}")
+                    answer = maybe_answer
+                    
+                    break 
+                cumulative_text += generation
+
+            else:
+                try: 
+                    code_output = process_code("```\n"+generation)
+                except Exception as e:
+                    code_output = str(e)
+                cumulative_text += "```output\n" + code_output + "\n```"
+
+        if answer:
+            answers.append(maybe_answer)
+        else:
+            # use the last code output as a potential answer
+            answers.append(code_output)
+    
+    return sample_best_answer(answers)
+
+def attempt_training_problem(csv_file, number):
+    with open(csv_file, 'r') as file:
+        # Create a reader object
+        csv_reader = csv.DictReader(file)
+        # find the problem and answer for the given number
+        for i, row in enumerate(csv_reader):
+            if i+1 == number:
+                problem = row['problem']
+                answer = row['answer']
+                print(f"Problem: {problem}")
+                print(f"True answer: {answer}")
+                return predict(problem)
+
+
+if __name__ == '__main__':
+    # read arguments and parse them
+    import argparse
+    parser = argparse.ArgumentParser()
+    # read the input number
+    parser.add_argument('--input', type=int, required=True)
+    args = parser.parse_args()
+
+
+    # Path to your CSV file
+    csv_file_path = "train.csv"
+
+    # List to hold each problem and answer
+    answers = []
+
+    for i in range(15):
+        answer = attempt_training_problem(csv_file_path, args.input)
+        print(f"Predicted Answer: {answer}")
+
+    count = 1
+    for answer in answers:
+        print(f"Answer {count}: {answer}")
+        count += 1
