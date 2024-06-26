85b5d2a1f1f49aa195b607c7207c1a578d25bf8b
Abhav Kedia <abhavkedia@gmail.com>
2024-06-26 15:06:42 -0700

    hello

--- training_valuemodel.py
+++ training_valuemodel.py
@@ -1,6 +1,6 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
-from huggingface_api import tokenizer
+from huggingface_api import tokenizer, value_model
 from pandas import read_csv
 
 class TextValueDataset(Dataset):
@@ -32,4 +32,33 @@ trainset = read_csv('training_data.csv')
 # Assuming you have a tokenizer for your base model
 dataset = TextValueDataset(trainset['text'], trainset['value'], tokenizer)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+def train_value_model(dataloader, model):
+    # freeze base model
+    for param in model.base_model.parameters():
+        param.requires_grad = False
+
+    model.train()
+    # Assuming 'dataloader' and 'value_model' are defined and 'value_model' is already loaded
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    criterion = torch.nn.MSELoss()
+
+    num_gpus = torch.cuda.device_count()
+    last_gpu = f'cuda:{num_gpus-1}'
+
+    for epoch in range(10):
+        running_loss=0.0
+        for inputs, labels in dataloader:
+                inputs = inputs.to('cuda:0')
+                labels = labels.to(last_gpu)
+                optimizer.zero_grad()
+                # fwd pass
+                outputs = model(inputs)
+                outputs = outputs.to(last_gpu)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+        print(f'Epoch {epoch+1}, Loss: {running_loss/len(dataloader)}')
+
 
