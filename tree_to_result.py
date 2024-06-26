c8cabc2551392c6ea6e97a8b8d6b7ede3be8044c
Abhav Kedia <abhavkedia@gmail.com>
2024-06-25 20:17:58 -0700

    improved treeresult

--- tree_to_result.py
+++ tree_to_result.py
@@ -1,4 +1,13 @@
 from training_runall import TreeNode, Tree
+import pickle
 
 def tree_to_result(tree):
     tree.print_tree()
+
+def save_tree(tree, file_name):
+    with open(file_name, 'wb') as file:
+        pickle.dump(tree, file)
+
+def load_tree(file_name):
+    with open(file_name, 'rb') as file:
+        return pickle.load(file)
